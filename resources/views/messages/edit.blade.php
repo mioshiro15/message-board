55044d70c01d5c27559d4ae6f9c2af45f0b81583
EC2 Default User <ec2-user@ip-172-31-35-181.us-east-2.compute.internal>
2018-05-22 07:19:12 +0000

    add column

--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -4,13 +4,25 @@
 
     <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
 
+<div class="row">
+        <div class="col-xs-6">
+
     {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
-        
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::submit('更新') !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
 
-    {!! Form::close() !!}
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+
+            {!! Form::close() !!}
+            
+</div>
+    </div>
 
 @endsection
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -4,14 +4,36 @@
 
     <h1>メッセージ一覧</h1>
 
-    @if (count($messages) > 0)
-        <ul>
-            @foreach ($messages as $message)
-                <li>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!} : {{ $message->content }}</li>
-            @endforeach
-        </ul>
-    @endif
+<div class="row">
+        <div class="col-xs-12"></div>
+        <div class="col-sm-offset-2 col-sm-6"></div>
+        <div class="col-md-offset-2 col-md-6"></div>
+        <div class="col-lg-offset-3 col-lg-3"></div>
 
-     {!! link_to_route('messages.create', '新規メッセージの投稿') !!}
+    @if (count($messages) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タイトル</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($messages as $message)
+                    <tr>
+                        <td>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!}</td>
+                        <td>{{ $message->title }}</td>
+                        <td>{{ $message->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    @endif
+    {!! link_to_route('messages.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+
+</div>
+    </div>
 
 @endsection
